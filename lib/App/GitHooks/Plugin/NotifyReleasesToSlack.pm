3394f15bc13b8f22d81b7fbce32fc47c8a55890a
Guillaume Aubert <aubertg@cpan.org>
2015-06-23 22:53:13 -0700

    Release version 1.1.0.

--- lib/App/GitHooks/Plugin/NotifyReleasesToSlack.pm
+++ lib/App/GitHooks/Plugin/NotifyReleasesToSlack.pm
@@ -88,11 +88,11 @@ Watch the notification appear in the corresponding Slack channel(s):
 
 =head1 VERSION
 
-Version 1.0.2
+Version 1.1.0
 
 =cut
 
-our $VERSION = '1.0.2';
+our $VERSION = '1.1.0';
 
 
 =head1 CONFIGURATION OPTIONS
